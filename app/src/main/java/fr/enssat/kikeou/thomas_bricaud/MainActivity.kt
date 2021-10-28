85092298f249e93dc1bc1148c130f16eefc55426
davidthomas <david.thomas2208@gmail.com>
2021-10-28 10:57:18 +0200

    Initial commit

---
+++ app/src/main/java/fr/enssat/kikeou/thomas_bricaud/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.enssat.kikeou.thomas_bricaud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
