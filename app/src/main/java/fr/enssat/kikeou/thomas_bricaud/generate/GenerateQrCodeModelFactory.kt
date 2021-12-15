b4ff8c3306cb0b85c8033c2eca4f95d089567b98
David THOMAS <david.thomas2208@gmail.com>
2021-12-15 11:30:51 +0100

    :hammer: update with view model binding

--- app/src/main/java/fr/enssat/kikeou/thomas_bricaud/generate/GenerateQrCodeModelFactory.kt
+++ app/src/main/java/fr/enssat/kikeou/thomas_bricaud/generate/GenerateQrCodeModelFactory.kt
@@ -4,10 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.lang.IllegalArgumentException
 
-class GenerateQrCodeModelFactory (private val name: String): ViewModelProvider.Factory {
+class GenerateQrCodeModelFactory (
+    private val name: String,
+    private val email: String,
+    private val phone: String,
+    private val monday: String,
+    private val tuesday: String,
+    private val wednesday: String,
+    private val thursday: String,
+    private val friday: String,
+    private val saturday: String,
+    ): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(GenerateQrCodeModel::class.java)) {
-            return GenerateQrCodeModel(name) as T
+            return GenerateQrCodeModel(
+                name, email, phone, monday, tuesday, wednesday, thursday, friday, saturday
+            ) as T
         }
         throw IllegalArgumentException("Unknow ViewModel Class")
     }
